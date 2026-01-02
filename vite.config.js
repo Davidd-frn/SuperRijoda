cef90d560220ce14a1bb95d4b8dbe3a910061ce2
SneeQz <rinor.murtezani@students.hevs.ch>
2026-01-02 14:49:28 +0100

    reset of modification

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve'
-  return {
-    plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
 
-    // Dev runs at root, build targets GitHub Pages under /game/
-    base: isDev ? '/' : '/game/',
-    build: {
-      outDir: 'docs',
-    },
-  }
-})
+  base: "/SuperRijoda/",
+  build: {
+    outDir: "docs",
+  },
+});
